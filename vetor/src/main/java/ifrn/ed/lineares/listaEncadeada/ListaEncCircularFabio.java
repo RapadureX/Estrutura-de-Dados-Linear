822e21b4012d9b3ae9a7ee3eab30db94d3974771
RapadureX <fabiomiguellima.rn@gmail.com>
2019-09-17 19:15:50 -0300

    Adicionando mais comentarios.

--- vetor/src/main/java/ifrn/ed/lineares/listaEncadeada/ListaEncCircularFabio.java
+++ vetor/src/main/java/ifrn/ed/lineares/listaEncadeada/ListaEncCircularFabio.java
@@ -72,27 +72,73 @@ public class ListaEncCircularFabio {
 		ultimo.setProximo(primeiro);
 	}
 	
+	/*
+	 * Método de buscar, que recebe como parâmetro um objeto do 
+	 * tipo aluno, que percorrerá a estrutura da lista até encontrar o aluno.
+	*/
 	public NoFabio buscar(Aluno aluno) throws ElementoInexistenteException, ListaVaziaException{
 		
+		//Condicional: Se a lista estiver vazia lance a exceção ListaVaziaException.
 		if(this.listaVazia()) {
 			throw new ListaVaziaException();
 		}
 		
-		NoFabio aux = primeiro.getProximo();
-		
+		/*
+		 * Condicional: Se o primeiro nó da lista que contém 
+		 * um aluno for igual ao aluno passado por parâmentro
+		 * , me retorne o primeiro nó.
+		 */
 		if(primeiro.getInfo().equals(aluno)) {
+			
+			// Retornando o primeiro nó da lista.
 			return primeiro;
+		
+		/*
+		* Se não mais um condicional: Se o último nó da lista que contém 
+		* um aluno for igual ao aluno passado por parâmentro
+		* , me retorne o último nó.
+		*/	
 		}else if(ultimo.getInfo().equals(aluno)){
+			
+			// Retornando o último nó da lista.
 			return ultimo;
-		}else {	
+			
+		/*
+		* Se não fará uma busca no restante da lista 
+		* até encontrar o nó que contenha o alunos 
+		* desejado.
+		*/		
+		}else {
+			
+			/* Nó auxiliar que receberá iniciamente 
+			 * o próximo nó do primeiro nó, que será utilizada 
+			 * para percorrer a lista.
+			 */
+			NoFabio aux = primeiro.getProximo();
+			
+			/* Estrutura de repetição: Enquanto o nó auxiliar 
+			 * for diferente do último nó, percorra a lista. 
+			 */
 			while(aux != ultimo) {
+				
+				/* Condicional: Se o aluno que está contido no nó auxiliar
+				 * for igual ao aluno passado por parâmetro, retorne o nó
+				 * auxiliar.
+				 */
 				if(aux.getInfo().equals(aluno)) {
+					
+					// Retronando o nó auxiliar.
 					return aux;
 				}
+				
+				// Não é o nó do aluno desejado, passe para o próximo nó.
 				aux = aux.getProximo();
 			}
 		}
 		
+		/* O aluno não foi encontrado na lista a exceção 
+		 * ElementoInexistenteException será lançada.
+		 */
 		throw new ElementoInexistenteException();
 	}
 	
